79ae9564da3b360fbb31bb751aa2dac7f9694670
yeremiya09 <kimjw0593@gmail.com>
2024-11-09 14:40:49 +0900

    feat: chapter_01 과제 업로드

---
+++ ass/config/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.http import Http404
+from django.shortcuts import render
+from fake_db import user_db
+
+
+_db = user_db
+
+def user_list(request):
+    names = [{'id': key, 'name': value['이름']} for key, value in _db.items()]
+    return render(request, 'user_list.html', {'data': names})
+
+def user_info(request, user_id):
+    if user_id > len(_db):
+        raise Http404('User not found')
+    info = _db[user_id]
+    return render(request, 'user_info.html', {'data': info})
+
+
+urlpatterns = [
+    path('users/', user_list, name='user_list'),
+    path('users/<int:user_id>/', user_info, name='user_info'),
+
+    path("admin/", admin.site.urls),
+]
